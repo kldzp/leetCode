20994c0410460d5309c65e3fba92697bdb2b1af9
zp <kldzp@126.com>
2018-05-24 23:17:44 +0800

    compare to best solution

--- src/Leet/leet14.java
+++ src/Leet/leet14.java
@@ -1,10 +1,16 @@
 package Leet;
 
+import java.sql.Time;
+
 public class leet14 {
 	public static void main(String[] args) {
-		String[] strings = {"flower","flow","flight",""};
+		String[] strings = {"flower","flow","flight"};
+		long start = System.nanoTime();
 		String result = findCommonPrefix(strings);
-		System.out.println(result);
+		long mid = System.nanoTime();
+		String leet = longestCommonPrefix(strings);
+		long end = System.nanoTime();
+		System.out.println((mid-start)-(end-mid));
 	}
 	public static String findCommonPrefix(String[] input) {
 		if (input.length == 0) {
@@ -28,6 +34,16 @@ public class leet14 {
 			
 		}
 		return result;
+	}
+	public static String longestCommonPrefix(String[] strs) {
+	    if (strs.length == 0) return "";
+	    String prefix = strs[0];
+	    for (int i = 1; i < strs.length; i++)
+	        while (strs[i].indexOf(prefix) != 0) {
+	            prefix = prefix.substring(0, prefix.length() - 1);
+	            if (prefix.isEmpty()) return "";
+	        }        
+	    return prefix;
 	}
 
 }
